a5c69a01ce303b19251fed2e288b9ed0736322a0
Miguel Angel Alfonso <105066751+MiguelAlfonso1723@users.noreply.github.com>
2022-07-04 01:50:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "Syntax.h"
+#include "Calculator.h"
 
 using namespace std;
 int main() {
-
+    Calculator calculator;
     cout<<"Calculadora"<<endl;
     while (true){
         cout<<"Escriba una operacion o escriba exit para salir"<<endl;
@@ -12,7 +13,7 @@ int main() {
         if(op != "exit"){
             Syntax *sin = new Syntax();
             if (sin->isCorrect(op)){
-
+                cout<<"El resultado de la operacion  "<<op<<"  es igual a:\n"<<fixed<<calculator.calculate("67*6+(12-23)/5**(4*7/3)", false)<<endl;
             }else{
                 cout<<"SYNTAX ERROR"<<endl;
             }
